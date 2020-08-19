acaedfe946f14bf88eaa22bba0d88fa8363d70a7
stefanleun <stefan.leun@mail.de>
2020-08-19 22:15:59 +0200

    Attempt to fix navigation on small devices

--- components/Header.vue
+++ components/Header.vue
@@ -8,43 +8,25 @@
         aria-label="menu"
         aria-expanded="false"
         data-target="navbarBasicExample"
+        v-on:click="showNav = !showNav"
+        v-bind:class="{ 'is-active' : showNav }"
       )
         span(aria-hidden="true")
         span(aria-hidden="true")
         span(aria-hidden="true")
-    .navbar-menu#navbarBasicExample
+    .navbar-menu#navbarBasicExample(
+      v-bind:class="{ 'is-active' : showNav }"
+    )
       .navbar-start
         NavLinks
 </template>
 
 <script>
   export default {
-    mounted() {
-      document.addEventListener('DOMContentLoaded', () => {
-
-        // Get all "navbar-burger" elements
-        const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
-
-        // Check if there are any navbar burgers
-        if ($navbarBurgers.length > 0) {
-
-          // Add a click event on each of them
-          $navbarBurgers.forEach( el => {
-            el.addEventListener('click', () => {
-
-              // Get the target from the "data-target" attribute
-              const target = el.dataset.target;
-              const $target = document.getElementById(target);
-
-              // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-              el.classList.toggle('is-active');
-              $target.classList.toggle('is-active');
-
-            });
-          });
-        }
-
-      });
+    data: function () {
+      return {
+        showNav: false
+      }
     }
   }
 </script>
