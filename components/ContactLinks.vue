81a36235bda0632cf804d32760171b81dbd68daf
Stefan Leun <stefan.leun@de.ey.com>
2020-10-12 13:16:57 +0200

    Add contact links to header on mobile devices

--- components/ContactLinks.vue
+++ components/ContactLinks.vue
@@ -1,15 +1,27 @@
 <template lang="pug">
   div
     a(href="mailto:stefan211.so@googlemail.com").contact-link
-      span.icon.is-large
+      span.icon.contact-icon
         ion-icon(size="large", name="mail")
     a(href="https://www.linkedin.com/in/stefanleun", target="_blank", rel="noopener noreferrer").contact-link
-      span.icon.is-large
+      span.icon.contact-icon
         ion-icon(size="large", name="logo-linkedin")
     a(href="https://www.xing.com/profile/Stefan_Leun/", target="_blank", rel="noopener noreferrer").contact-link
-      span.icon.is-large
+      span.icon.contact-icon
         ion-icon(size="large", name="logo-xing")
     a(href="https://github.com/stefanleun", target="_blank", rel="noopener noreferrer").contact-link
-      span.icon.is-large
+      span.icon.contact-icon
         ion-icon(size="large", name="logo-github")
 </template>
+
+<style scoped lang="sass">
+@import "~/node_modules/bulma/sass/utilities/initial-variables.sass";
+@import "~/node_modules/bulma/sass/utilities/derived-variables";
+@import "~/node_modules/bulma/sass/utilities/mixins.sass";
+@import "~/node_modules/bulma/sass/elements/icon.sass";
+
+span.icon.contact-icon
+  @extend .is-large
+  +touch
+    height: 1.8rem;
+</style>
--- components/Header.vue
+++ components/Header.vue
@@ -3,6 +3,8 @@
     .navbar-brand
       a.navbar-item(href="https://stefanleun.github.io")
         Logo
+      .navbar-item
+        ContactLinks
       a.navbar-burger.burger(
         role="button"
         aria-label="menu"
